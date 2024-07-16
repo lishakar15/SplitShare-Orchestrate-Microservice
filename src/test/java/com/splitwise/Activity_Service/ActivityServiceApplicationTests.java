15bb5e526d73b7159999d9215ff77658777a4528
Lishakar <lishakarj@gmail.com>
2024-07-16 22:34:23 +0530

    Activity-Service

---
+++ src/test/java/com/splitwise/Activity_Service/ActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.Activity_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
