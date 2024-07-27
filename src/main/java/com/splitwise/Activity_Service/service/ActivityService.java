b04301a9bf63a122de7b4adfb77c0e595f17d4ea
Lishakar <lishakarj@gmail.com>
2024-07-28 00:49:28 +0530

    Replace userId with userName for Activities done

--- src/main/java/com/splitwise/Activity_Service/service/ActivityService.java
+++ src/main/java/com/splitwise/Activity_Service/service/ActivityService.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ActivityService {
@@ -31,6 +33,7 @@ public class ActivityService {
             //Get UserNames from Cache
             Map<Long, String> userNameMap = cacheService.getUserNameMap(groupId);
             userNameMap.forEach((k,v)-> System.out.println("Key = "+k+" value = "+v.toString()));
+            getActivitiesWithUserName(activityList,userNameMap);
         }
         //Need to replace the userId with userName in Message
         return activityList;
@@ -70,4 +73,32 @@ public class ActivityService {
             }
         }
     }
+    public List<Activity> getActivitiesWithUserName(List<Activity> activities, Map<Long, String> userNameMap )
+    {
+        if(activities != null || userNameMap != null)
+        {
+            for(Activity activity : activities) {
+                String message = activity.getMessage();
+                if (message != null)
+                {
+                    Pattern pattern = Pattern.compile("\\{userId:(\\d+)\\}");//(\d+) matcher group for userId
+                    Matcher matcher = pattern.matcher(message);
+                    StringBuffer sb = new StringBuffer();
+                    while (matcher.find()) {
+                        Long userId = Long.parseLong(matcher.group(1));
+                        //Todo: Need to check if the userid is equal to logged in user and show "you" instead of name
+                        matcher.appendReplacement(sb, userNameMap.get(userId));
+                    }
+                    matcher.appendTail(sb);
+                    System.out.println(sb.toString());
+                    if(sb.length()>0)
+                    {
+                        activity.setMessage(sb.toString());
+                    }
+                }
+            }
+        }
+        return activities;
+    }
+
 }
