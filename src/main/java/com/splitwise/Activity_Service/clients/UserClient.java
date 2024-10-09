14936320e7b07bcaaff6fb5d451c3424466ffd56
Lishakar J <lishakarj@gmail.com>
2024-10-10 01:35:04 +0530

    AWS SQS client implemented

--- src/main/java/com/splitwise/Activity_Service/clients/UserClient.java
+++ src/main/java/com/splitwise/Activity_Service/clients/UserClient.java
@@ -4,12 +4,16 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "user-service", url = "http://localhost:8080/")
+@FeignClient(name = "USER-SERVICE")
 public interface UserClient {
 
     @GetMapping("/user/get-user-name-map/{groupId}")
     public Map<Long,String> getUserNameMap(@PathVariable("groupId") Long groupId);
+    @PostMapping("user/get-user-email-map")
+    public Map<Long,String> getUserEmailMapByUserIds(List<Long> userIds);
 }
